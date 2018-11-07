abe02ba7cb2085de46a38fe7e6e896103cd75819
mariomui <yi.kan.mui@gmail.com>
2018-11-07 12:57:41 -0800

    first commit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,39 @@
 var Stack = function() {
-  var someInstance = {};
+    var someInstance = {
+        counter: 0,
+        storage: {},
+    };
 
-  // Use an object with numeric keys to store values
-  var storage = {};
+    // var counter = 0;
 
-  // Implement the methods below
-  someInstance.push = function(value) {
-  };
+    // Use an object with numeric keys to store values
+    // var storage = {};
+    // Implement the methods below
+    someInstance.push = function(value) {
+        // var string = '' + someInstance.counter;
+        // console.log('*', value);
+        someInstance.storage[someInstance.counter] = value;
+        someInstance.counter++;
+    };
 
-  someInstance.pop = function() {
-  };
+    someInstance.pop = function() {
+        var end = someInstance.size() - 1;
 
-  someInstance.size = function() {
-  };
+        console.log('popping location', end);
+        var result = someInstance.storage[end];
+        delete someInstance.storage[end];
+        someInstance.counter--;
+        return result;
 
-  return someInstance;
+    };
+
+    someInstance.size = function() {
+        var result = someInstance.counter;
+        if (result < 0) {
+            return 0;
+        }
+        return result || 0;
+    };
+
+    return someInstance;
 };

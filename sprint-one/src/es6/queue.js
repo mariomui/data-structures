619ccb6e7f708c1b439ccff53a2d2b637c9140e5
mariomui <yi.kan.mui@gmail.com>
2018-11-07 17:08:46 -0800

    Gold Copy

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,43 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
+    // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+    // but try not not reference your old code in writing the new style.
+    constructor() {
+        this.magic = 0;
+        this.offSet = 0;
+        this.length = 0;
+        this.storage = {};
+
+    }
+    enqueue(value) {
+        this.length++;
+        this.storage[this.offSet] = value;
+        console.log(this.offSet, '!');
+        console.log(length);
+        this.offSet++;
+    };
+
+    dequeue() {
+        if (this.length !== 0) {
+            this.length--;
+        }
+        var temp = this.storage[this.magic];
+        this.magic++;
+
+        return temp;
+    };
+
+    size() {
+        // if (!size) {
+        //     return 0;
+        // }
+        // if (this.length < 0) {
+        //     this.length = 0;
+        //     return this.length;
+
+        // }
+        return this.length || 0;
+    };
 
 }
+
+var jack = Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,36 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
+    // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+    // but try not not reference your old code in writing the new style.
+    constructor() {
+        this.storage = {};
+        this.counter = 0;
+    }
+    push(value) {
+        // var string = '' + someInstance.counter;
+        // console.log('*', value);
+        this.storage[this.counter] = value;
+        this.counter++;
+    };
 
+
+    pop() {
+        var end = this.size() - 1;
+
+        console.log('popping location', end);
+        var result = this.storage[end]; // >> 
+        delete this.storage[end];
+        this.counter--;
+        return result;
+
+    };
+    size() {
+        var result = this.counter;
+
+        if (result <= 0 || result === undefined) {
+            return 0;
+        }
+        return result || 0;
+    }
 }
+
+var boo = new Stack();

25d94f23bddba415efbc5b7858eb16db7c5e8e2b
mariomui <yi.kan.mui@gmail.com>
2018-11-10 11:23:50 -0800

    Binary Search Tree Completed. Beginning Refactors
    We completed all six algorithms and tests run correctly. Beginning
    approach on adding functionality and tests.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -6,36 +6,80 @@ var BinarySearchTree = function(value) {
    return obj;
 };
 
-// var node = function (value){
-//     var nodes = {};
-//     nodes.left = null;
-//     nodes.right = null;
-//     nodes.value = value;
-//     return nodes
-// }
 
 //this : binarysearchbaby
 BinarySearchTree.prototype.insert = function(value) {
     //create the node
     //insert it in left or right
-
-    let bstBaby = node(value); 
+    //bstBaby = the node usually in this.right
+    let bstBaby = BinarySearchTree(value); 
     if (value > this.value) {
-        this.right = bstBaby;
+        //if right node exists, go into the object right node and run insert using the value.
+        if (this.right === null) {
+            
+            this.right = bstBaby;
+        }
+        this.right.insert(value);
+
     } else if (value < this.value) {
-        this.left = bstBaby;
+        // this.left = bstBaby;
+        if (this.left === null) {
+            
+            this.left = bstBaby;
+        }
+        this.left.insert(value);
     } 
 
 
 }
 
-BinarySearchTree.prototype.contains = function(){
-    return 5;
+BinarySearchTree.prototype.contains = function(target){
+    //base case
+    
+     if ( target === this.value) {
+         return true;
+     } 
+     //recursive case
+     if ( this.left || this.right) {
+         //if small do recurse on left
+         if (this.value > target ) {
+             return this.left.contains(target);
+
+         } else if (this.value < target) {
+             return this.right.contains(target);
+         }
+         //i big do recurse on right
+         
+     }  
+     return false;
+
+    //recurseive case
 }
 
-BinarySearchTree.prototype.depthFirstLog = function(){
-    return 7;
+//input func
+//output do some work
+BinarySearchTree.prototype.depthFirstLog = function(func){
+    //base case no children
+    func(this.value);
 
+    // if ( !this.left && !this.right) {
+    //     //do nothing
+    //     return;
+    // }
+
+    if (this.left) {
+        //recurse
+        // func(this.left.value);
+        this.left.depthFirstLog(func);
+        
+    } 
+    if (this.right) {
+        //recurse
+        // func(this.right.value);
+        this.right.depthFirstLog(func);
+    }
+    //recursive case
+    return;
 }
 
 
